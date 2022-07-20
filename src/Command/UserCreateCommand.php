47c55a4c17dfee685fa1ba115e1073620425bf52
Samuel Belolo <contact@samuelbelolo.com>
2022-07-20 11:35:42 +0200

    :sparkles: added user creation command

--- src/Command/UserCreateCommand.php
+++ src/Command/UserCreateCommand.php
@@ -2,42 +2,70 @@
 
 namespace App\Command;
 
+use App\Document\User;
+use App\Utils\Validator;
+use Doctrine\ODM\MongoDB\DocumentManager;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
-#[AsCommand(
-    name: 'app:user:create',
-    description: 'Add a short description for your command',
-)]
+#[AsCommand(name: 'app:user:create', description: 'Create a User')]
 class UserCreateCommand extends Command
 {
+    public function __construct(
+        protected DocumentManager $dm,
+        protected Validator $validator,
+        protected UserPasswordHasherInterface $passwordHasher
+    ) {
+        parent::__construct();
+    }
+
     protected function configure(): void
     {
         $this
-            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
-            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+            ->addArgument('email', InputArgument::REQUIRED, 'Email of the user to create')
+            ->addArgument('password', InputArgument::REQUIRED, 'Password of the user to create')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $io   = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
+        $io       = new SymfonyStyle($input, $output);
+        $email    = $input->getArgument('email');
+        $password = $input->getArgument('password');
 
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
+        $io->section('Creating User in db');
+
+        if ($email) {
+            $io->note(sprintf('Email : %s', $email));
+        }
+        if ($password) {
+            $io->note(sprintf('Password : %s', $password));
         }
 
-        if ($input->getOption('option1')) {
-            // ...
+        $user           = new User();
+        $hashedPassword = $this->passwordHasher->hashPassword($user, $password);
+        $user->setPassword($hashedPassword);
+
+        $user->setEmail($email);
+
+        $errors = $this->validator->getErrors($user);
+
+        if (count($errors) > 0) {
+            $errors = call_user_func_array('array_merge', array_values($errors));
+            $io->error($errors);
+
+            return Command::FAILURE;
         }
 
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+        $this->dm->persist($user);
+        $this->dm->flush();
+
+        $io->success('User created !');
 
         return Command::SUCCESS;
     }
