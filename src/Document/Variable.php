41b1abf950f512e1a0c820bcc1b4afba5b81fc6a
Lucas Lehot <lehotlucas@gmail.com>
2022-07-21 11:05:35 +0200

    🚧 add variables crud

---
+++ src/Document/Variable.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Document;
+
+use Doctrine\ODM\MongoDB\Mapping\Annotations as MongoDB;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[MongoDB\Document(collection: 'variables')]
+class Variable 
+{
+    #[MongoDB\Id]
+    protected string $id;
+
+    #[MongoDB\Field(type: 'string')]
+    #[Assert\NotBlank]
+    protected string $projectId;
+
+    #[MongoDB\Field(type: 'string')]
+    #[Assert\NotBlank]
+    protected ?string $name = null;
+
+    #[MongoDB\Field(type: 'string')]
+    #[Assert\NotBlank]
+    #[Assert\Json]
+    protected ?string $data = null;
+
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function getProjectId(): string
+    {
+        return $this->projectId;
+    }
+
+    public function setProjectId(?string $projectId): void
+    {
+        $this->projectId = $projectId;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getData(): ?string
+    {
+        return $this->data;
+    }
+
+    public function setData(?string $data): void
+    {
+        $this->data = $data;
+    }
+}
