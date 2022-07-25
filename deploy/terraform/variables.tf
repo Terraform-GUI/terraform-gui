1c9e082061b2f8c2f2b88b038966ef54ea9a3ecc
blyndusk <alexandre.delaloy.pro@gmail.com>
2022-07-25 17:52:42 +0200

    add(terraform): #14 - impl

---
+++ deploy/terraform/variables.tf
@@ -0,0 +1,49 @@
+variable "global" {
+  type = map(string)
+  default = {
+    name   = "tfgui-alpha"
+    region = "us-east-1"
+  }
+}
+
+variable "vpc" {
+  type = map(string)
+  default = {
+    tenancy    = "default"
+    cidr       = "10.0.0.0/24"
+    pub_subnet = "10.0.0.128/26"
+    prv_subnet = "10.0.0.192/26"
+    eip_vpc    = true
+  }
+}
+
+variable "ec2" {
+  type = map(string)
+  default = {
+    ami       = "ami-052efd3df9dad4825"
+    type      = "t2.micro"
+    public_ip = true
+  }
+}
+
+variable "s3" {
+  type = map(string)
+  default = {
+    acl = "private"
+  }
+}
+
+variable "rds" {
+  type = map(string)
+  default = {
+    type                = "db.t3.micro"
+    engine              = "postgres"
+    version             = "1.13"
+    family             = "postgres13"
+    username            = "alpha"
+    password            = "alpha"
+    publicly_accessible = true
+    skip_final_snapshot = true
+  }
+}
+ 
