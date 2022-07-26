06f686b59d6e6625143fa43574a69a9f555fcc69
Lucas Lehot <lehotlucas@gmail.com>
2022-07-26 18:37:45 +0200

    feat(api): #6 - handling email update

--- api/src/Controller/Api/UserController.php
+++ api/src/Controller/Api/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Api;
 
+use App\Document\EmailUpdateRequest;
 use App\Document\PasswordRecoveryRequest;
 use App\Document\User;
 use App\Form\UserUpdateEmailType;
@@ -11,42 +12,66 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/api', name: 'api_user_')]
 class UserController extends AbstractController
 {
-    #[Route('/users/{user_id}', name: 'update_email', methods: ['PUT'])]
-    public function updateEmail(Request $request, DocumentManager $dm, Validator $validator, $user_id): JsonResponse
+    #[Route('/users/{user_id}/request-email-update', name: 'request_update_email', methods: ['POST'])]
+    public function requestEmailUpdate(Request $request, DocumentManager $dm, Validator $validator, MailerInterface $mailer): JsonResponse
     {
-        $user = $dm->getRepository(User::class)->find($user_id);
-        if (!$user) {
-            return $this->json(['error' => 'User not found'], Response::HTTP_NOT_FOUND);
-        }
-
         $data = json_decode($request->getContent(), true);
+
         $form = $this->createForm(UserUpdateEmailType::class);
-        $form->submit($data);
+
+        $form->submit(array_merge($data, $request->request->all()));
 
         if ($form->isSubmitted() && $form->isValid()) {
             $formData = $form->getData();
 
-            $user->setEmail($formData['email']);
+            $user = $dm->getRepository(User::class)->findOneBy(['email' => $formData['email']]);
 
-            $errors = $validator->getErrors($user);
+            if (null === $user) {
+                return $this->json(['errors' => ['email' => 'User not found']], Response::HTTP_UNPROCESSABLE_ENTITY);
+            }
 
-            if (count($errors) > 0) {
-                return $this->json(['errors' => $errors], Response::HTTP_UNPROCESSABLE_ENTITY);
+            $emailUpdateRequest = $dm
+                ->getRepository(EmailUpdateRequest::class)
+                ->findOneBy(
+                    ['email' => $formData['email'], 'used' => false],
+                    ['createdAt' => 'desc']
+                )
+            ;
+
+            if (null === $emailUpdateRequest) {
+                $emailUpdateRequest = new EmailUpdateRequest();
+                $emailUpdateRequest->setEmail($formData['email']);
+                $emailUpdateRequest->setNewEmail($formData['new_email']);
+                $emailUpdateRequest->setToken(bin2hex(random_bytes(32)));
+                $dm->persist($emailUpdateRequest);
+            } else {
+                $emailUpdateRequest->setNewEmail($formData['new_email']);
+                $emailUpdateRequest->setToken(bin2hex(random_bytes(32)));
+                $emailUpdateRequest->setCreatedAt(new \DateTimeImmutable());
             }
 
             $dm->flush();
 
-            return $this->json(['success' => true], Response::HTTP_OK);
+            $email = (new Email())
+                ->from($_ENV['MAILER_FROM_EMAIL'])
+                ->to($emailUpdateRequest->getNewEmail())
+                ->subject('Verify your email address')
+                ->text('Verify your email address')
+                ->html('<p>You have requested an email update. Please click the following link to confirm your new email address.</p> <a href="'.$_ENV['FRONT_URL'].'users/'.$user->getId().'/email-update?token='.$emailUpdateRequest->getToken().'">Confirm your email</a>');
+
+            $mailer->send($email);
+
+            return $this->json(['success' => true], Response::HTTP_CREATED);
         }
 
-        $errors = $validator->getErrors($form, false);
-
-        return $this->json(['errors' => $errors], Response::HTTP_UNPROCESSABLE_ENTITY);
+        return $this->json(['errors' => $validator->getErrors($form)], Response::HTTP_UNPROCESSABLE_ENTITY);
     }
 
     #[Route('/users/{user_id}', name: 'delete_user', methods: ['DELETE'])]
--- api/src/Controller/UserController.php
+++ api/src/Controller/UserController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Client\Github;
+use App\Document\EmailUpdateRequest;
 use App\Document\User;
 use App\Form\UserRegistrationType;
+use App\Form\UserUpdateEmailType;
 use App\Utils\Validator;
 use Doctrine\ODM\MongoDB\DocumentManager;
 use Lexik\Bundle\JWTAuthenticationBundle\Security\Http\Authentication\AuthenticationSuccessHandler;
@@ -130,5 +132,68 @@ class UserController extends AbstractController
         $tokens = json_decode($tokens, true);
 
         return $this->redirect($_ENV['FRONT_URL'].'?'.http_build_query($tokens));
+    }
+
+    #[Route('/users/{user_id}/email-update', name: 'email_update', methods: ['POST'])]
+    public function updateEmail(Request $request, DocumentManager $dm, Validator $validator, MailerInterface $mailer): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $form = $this->createForm(UserUpdateEmailType::class);
+
+        $form->submit(array_merge($data, $request->request->all()));
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $formData = $form->getData();
+
+            $emailUpdateRequest = $dm
+                ->getRepository(EmailUpdateRequest::class)
+                ->findOneBy(
+                    ['token' => $formData['token'], 'used' => false],
+                    ['createdAt' => 'desc']
+                )
+            ;
+
+            if (null === $emailUpdateRequest) {
+                return $this->json(['errors' => 'Unauthorized'], Response::HTTP_UNAUTHORIZED);
+            }
+
+            if ($emailUpdateRequest->getCreatedAt()->diff(new \DateTimeImmutable())->s > (int) $_ENV['PASSWORD_RESET_EXPIRATION']) {
+                return $this->json(['errors' => 'Token expired'], Response::HTTP_UNAUTHORIZED);
+            }
+
+            $user = $dm->getRepository(User::class)->findOneBy(['email' => $emailUpdateRequest->getEmail()]);
+
+            if (null === $user) {
+                return $this->json(['errors' => 'Unauthorized'], Response::HTTP_UNAUTHORIZED);
+            }
+
+            $user->setEmail($emailUpdateRequest->getNewEmail());
+
+            $errors = $validator->getErrors($user);
+
+            if (count($errors) > 0) {
+                return $this->json(['errors' => $errors], Response::HTTP_UNPROCESSABLE_ENTITY);
+            }
+
+            $emailUpdateRequest->setUsed(true);
+
+            $dm->flush();
+
+            $email = (new Email())
+                ->from($_ENV['MAILER_FROM_EMAIL'])
+                ->to($user->getEmail())
+                ->subject('Your email has been updated')
+                ->text('Your email has been updated')
+                ->html('<p>Your email has been updated</p>');
+
+            $mailer->send($email);
+
+            return $this->json(['success' => true], Response::HTTP_CREATED);
+        }
+
+        $errors = $validator->getErrors($form, false);
+
+        return $this->json(['errors' => $errors], Response::HTTP_UNPROCESSABLE_ENTITY);
     }
 }
