8d6dcb024f2d51eb96f052483460716230c10c18
Lucas Lehot <lehotlucas@gmail.com>
2022-07-26 17:31:56 +0200

    fix(api): #6 - applying review

--- api/src/Controller/Api/VariableController.php
+++ api/src/Controller/Api/VariableController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route(name: 'variable_')]
+#[Route('/api', name: 'api_variables_')]
 class VariableController extends AbstractController
 {
     #[Route('/projects/{project_id}/variables', name: 'get-variables', methods: ['GET'])]
