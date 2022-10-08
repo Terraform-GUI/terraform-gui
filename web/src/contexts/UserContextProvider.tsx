39a5312cf426190211cbe2b57b1ee77f6ce1c4c8
Lucas Lehot <lehotlucas@gmail.com>
2022-10-08 15:52:56 +0200

    upd(front): #20 - connecting context and localstorage

--- web/src/contexts/UserContextProvider.tsx
+++ web/src/contexts/UserContextProvider.tsx
@@ -1,4 +1,4 @@
-import React, {FunctionComponent, useState} from "react";
+import React, {FunctionComponent, useState, useEffect} from "react";
 
 import UserContext from "./UserContext";
 
@@ -8,8 +8,17 @@ interface IUserContextProvider {
 
 const UserContextProvider: FunctionComponent<IUserContextProvider> = ({children}) => {
     const [email, setEmail] = useState<string>('');
-    const [accessToken, setAccessToken] = useState<string>('false');
-    const [refreshToken, setRefreshToken] = useState<string>('false');
+    const [accessToken, setAccessToken] = useState<string>('');
+    const [refreshToken, setRefreshToken] = useState<string>('');
+
+    localStorage.getItem('access_token') && setAccessToken(localStorage.getItem('access_token')!);
+    localStorage.getItem('refresh_token') && setRefreshToken(localStorage.getItem('refresh_token')!);
+    useEffect(() => {
+        localStorage.setItem('is-access_token', accessToken);
+    }, [accessToken]);
+    useEffect(() => {
+        localStorage.setItem('is-refresh_token', refreshToken);
+    }, [refreshToken]);
 
     return (
         <UserContext.Provider
