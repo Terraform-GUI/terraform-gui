5e9feffe344750048f53b5ec001a7cebcdf20940
blyndusk <alexandre.delaloy.pro@gmail.com>
2022-10-11 16:34:44 +0200

    fix(samples): vars and arguments

--- samples/variables.tf
+++ samples/variables.tf
@@ -1,6 +1,7 @@
 variable "global" {
   type = map(string)
   default = {
+    # WARN: need to be alpha-lowercase with comma: NO spaces or Uppercase
     name   = "tfgui-alpha"
     region = "us-east-1"
   }
@@ -9,17 +10,22 @@ variable "global" {
 variable "vpc" {
   type = map(string)
   default = {
+    # WARN: need to be 'default'
     tenancy    = "default"
     cidr       = "10.0.0.0/24"
     pub_subnet = "10.0.0.128/26"
     prv_subnet = "10.0.0.192/26"
-    eip_vpc    = true
+    # to be added respectively in public and private
+    pub_availability_zone = "us-east-1a"
+    prv_availability_zone = "us-east-1b"
+    eip_vpc               = true
   }
 }
 
 variable "ec2" {
   type = map(string)
   default = {
+    # WARN: need to be specific IDs (maybe use select form)
     ami       = "ami-052efd3df9dad4825"
     type      = "t2.micro"
     public_ip = true
@@ -41,6 +47,7 @@ variable "rds" {
     version             = "13"
     family              = "postgres13"
     username            = "alpha"
+    # WARN: need to be > 8 characters
     password            = "alphaalphaalpha"
     publicly_accessible = false
     skip_final_snapshot = true
